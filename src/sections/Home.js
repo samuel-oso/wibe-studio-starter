3f0db4c9ff993d95a45c85482c0e6abdb93ec5eb
unknown <ososamuel035@gmail.com>
2022-07-01 14:36:39 +0100

    Logo and locomotive scroll correction

---
+++ src/sections/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+import CoverVideo from "../components/CoverVideo";
+import Logo from "../components/Logo";
+
+const Section = styled.section`
+  position: relative;
+  min-height: 200vh;
+  overflow: hidden;
+`;
+
+const Home = () => {
+  return (
+    <Section>
+      <CoverVideo />
+      <Logo />
+      <h1>NavBar</h1>
+    </Section>
+  );
+};
+
+export default Home;
